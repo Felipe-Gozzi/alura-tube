70fb55fb2697eaf6620891dfdcd3e6a8f6cc6a52
Felipe Gozzi <felipegozzi16@gmail.com>
2022-11-11 17:46:19 -0300

    Class 02 - add Search

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import config from "../config.json"
-import styled from "styled-components"
-import { CSSReset } from "../src/components/CSSReset"
-import Menu from "../src/components/Menu"
-import { StyledTimeline } from "../src/components/Timeline"
+import React from "react";
+import config from "../config.json";
+import styled from "styled-components";
+import { CSSReset } from "../src/components/CSSReset";
+import Menu from "../src/components/Menu";
+import { StyledTimeline } from "../src/components/Timeline";
 
 function HomePage() {
+    const [valorDoFiltro, setValorDoFiltro] = React.useState("");
+
     return (
         <>
             <CSSReset />
@@ -13,12 +16,14 @@ function HomePage() {
                 flexDirection: "column",
                 flex: 1,
             }}>
-                <Menu />
+                <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
                 <Header />
-                <Timeline playlists={config.playlists}></Timeline>
+                <Timeline searchValue={valorDoFiltro} playlists={config.playlists}>
+                    Conteúdo
+                </Timeline>
             </div>
         </>
-    )
+    );
 }
 
 export default HomePage
@@ -50,34 +55,44 @@ function Header() {
             <section className="user-info">
                 <img src={`https://github.com/${config.github}.png`} />
                 <div>
-                    <h2>{config.name}</h2>
-                    <p>{config.job}</p>
+                    <h2>
+                        {config.name}
+                    </h2>
+                    <p>
+                        {config.job}
+                    </p>
                 </div>
             </section>
         </StyledHeader>
     )
 }
 
-function Timeline(props) {
-    const playlistsNames = Object.keys(props.playlists);
+function Timeline({ searchValue, ...propriedades }) {
+    const playlistNames = Object.keys(propriedades.playlists);
     return (
         <StyledTimeline>
-            {playlistsNames.map((playlistName) => {
-                const videos = props.playlists[playlistName];
+            {playlistNames.map((playlistName) => {
+                const videos = propriedades.playlists[playlistName];
                 return (
-                    <section>
+                    <section key={playlistName}>
                         <h2>{playlistName}</h2>
                         <div>
-                            {videos.map((video) => {
-                                return (
-                                    <a href={video.url}>
-                                        <img src={video.thumb} />
-                                        <span>
-                                            {video.title}
-                                        </span>
-                                    </a>
-                                )
-                            })}
+                            {videos
+                                .filter((video) => {
+                                    const titleNormalized = video.title.toLowerCase();
+                                    const searchValueNormalized = searchValue.toLowerCase();
+                                    return titleNormalized.includes(searchValueNormalized)
+                                })
+                                .map((video) => {
+                                    return (
+                                        <a key={video.url} href={video.url} target="_blank">
+                                            <img src={video.thumb} />
+                                            <span>
+                                                {video.title}
+                                            </span>
+                                        </a>
+                                    )
+                                })}
                         </div>
                     </section>
                 )
